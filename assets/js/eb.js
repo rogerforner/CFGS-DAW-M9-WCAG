cd23762deddbccba0ecdfeea17f05de62c0f6e05
Enric Beltran Cano <enricbeltran@iesmontsia.org>
2018-02-17 12:40:57 +0100

    Algo ia te forma

--- assets/js/eb.js
+++ assets/js/eb.js
@@ -3,3 +3,27 @@
 #
 *******************************************************************************/
 $("nav a#eb").addClass("active");
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+function reFirstImg (){
+  window.location.replace("guideline1.1.1.1.html#section1");
+}
+
+function tempsExipre (){
+  alert("En 20 segons expira la cookie, espitja enter per cancelar");
+}
+
+$(document).ready(function(){
+    $("#start").click(function(){
+        $("#panel").slideDown(7000);
+    });
+    $("#stop").click(function(){
+        $("#panel").stop();
+    });
+    $("#up").click(function(){
+        $("#panel").slideUp(7000);
+    });
+});
